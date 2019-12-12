1b3232e986dcaf4e1df239790df221c4df8aa0c2
Vladimir <volody.grab@yandex.ru>
2019-12-12 23:32:49 +0500

    redact component App 12-12-2019

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import Typewindows from './components/Typewindows';
-import list from './components/list';
-;
+import {listWindows} from './components/list';
+import './static/style/app.css'
 
 
 
 
  export default class App extends React.Component{
     
+  constructor(props){
+    super(props)
+
+    this.state = {indexImg:1}
+
+  }
+
+
+  handleImg(index){
+    console.log(index)
+    this.setState({indexImg:index})
+    return index;
+  }
 
   render(){
-    if(!list) return false;
-
+    if(!listWindows) return false;
+      console.log(listWindows);
       return(
           <div className="container-widget-calc">
 
@@ -19,16 +32,18 @@ import list from './components/list';
 
                 <div> 
                     {
-                      list.map((item, index)=>(
-                        <div key={item.id}>
-                          <Typewindows imgwindows={item.imgwindows}  titlewind={item.titlewind} />
+                      listWindows.map((item)=>(
+                        <div key={item.id} onClick={()=>{this.handleImg(item.id)}}>
+                          <Typewindows imgwindows={item.imgwindows}   titlewind={item.titlewind} />
                         </div>
                       ))
                     }
                 </div>
 
                 <div> 
-                  
+                  {
+                    this.state.indexImg ? listWindows[this.state.indexImg].bigimg : "No photo"
+                  }
                 </div>
 
 
